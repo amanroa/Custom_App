fcc5edacc9c014a8593f01e9b9425ddae9ac4167
Aashni Manroa <aashni@Aashnis-MacBook-Pro.local>
2020-03-02 10:09:55 -0500

    Initial commit

---
+++ app/src/main/java/com/manroaaashni/custom_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.manroaaashni.custom_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //use shared pref to save usernames and score?
+    }
+}
