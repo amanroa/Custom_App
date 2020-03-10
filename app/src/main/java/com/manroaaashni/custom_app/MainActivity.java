e4655f9d64206843df1b78b6eeed16c8b1361dbe
aashni <aashni.manroa1@gmail.com>
2020-03-10 10:08:49 -0400

    6c - I added OnClickListeners to change the background color and button colors. I also fixed the bug that wasn't updating the numbers, and I tested my app to make sure that the points were updating accordingly.

--- app/src/main/java/com/manroaaashni/custom_app/MainActivity.java
+++ app/src/main/java/com/manroaaashni/custom_app/MainActivity.java
@@ -1,9 +1,13 @@
 package com.manroaaashni.custom_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import java.util.Random;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,9 +15,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
+    Button color_change;
+    Button bg_change;
     TextView num1;
     TextView num2;
     TextView points_txt;
+    ConstraintLayout cl;
     int points;
 
     Random rand = new Random();
@@ -21,64 +28,111 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //use shared pref to save usernames and score?
-    }
-    public void color_change(){
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        double rand_1 = rand.nextDouble();
-        if (rand_1 < 0.3){
-            button1.setBackgroundColor(getResources().getColor(R.color.purple));
-            button2.setBackgroundColor(getResources().getColor(R.color.purple));
-        }
-        else if (rand_1 >= 0.3  && rand_1 <0.6){
-            button1.setBackgroundColor(getResources().getColor(R.color.blue));
-            button2.setBackgroundColor(getResources().getColor(R.color.blue));
-        }
-        else{
-            button1.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-            button2.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        }
-    }
-    public void button_1_clicked(){
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        num1 = findViewById(R.id.num1);
-        num2 = findViewById(R.id.num2);
-        points_txt = findViewById(R.id.points_txt);
-        int val1 = Integer.parseInt((num1.getText()).toString());
-        int val2 = Integer.parseInt((num2.getText()).toString());
-        if (val1 >= val2){
-            points++;
-            points_txt.setText("Points: " + points);
-            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            points--;
-            points_txt.setText("Points: " + points);
-            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
+        new_rand_numbers();
+        //use shared pref to save user names and score?
+        bg_change = findViewById(R.id.bg_button);
+        bg_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        }
-    }
-    public void button_2_clicked(){
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        num1 = findViewById(R.id.num1);
-        num2 = findViewById(R.id.num2);
-        points_txt = findViewById(R.id.points_txt);
-        int val1 = Integer.parseInt((num1.getText()).toString());
-        int val2 = Integer.parseInt((num2.getText()).toString());
-        if (val1 <= val2){
-            points++;
-            points_txt.setText("Points: " + points);
-            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            points--;
-            points_txt.setText("Points: " + points);
-            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
+                cl = findViewById(R.id.cl);
+                double rand_1 = rand.nextDouble();
+                if (rand_1 < 0.2){
+                    cl.setBackgroundColor(Color.LTGRAY);
+                }
+                else if (rand_1 >= 0.2 && rand_1 < 0.4){
+                    cl.setBackgroundColor(Color.CYAN);
+                }
+                else if (rand_1 >= 0.4  && rand_1 <0.6){
+                    cl.setBackgroundColor(Color.YELLOW);
+                }
+                else if (rand_1 >= 0.6  && rand_1 <0.8){
+                    cl.setBackgroundColor(Color.MAGENTA);
+                }
+                else{
+                    cl.setBackgroundColor(Color.BLUE);
+                }
+            }
+        });
 
-        }
+
+        color_change = findViewById(R.id.color_change);
+        color_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1 = findViewById(R.id.button1);
+                button2 = findViewById(R.id.button2);
+                double rand_1 = rand.nextDouble();
+                if (rand_1 < 0.2){
+                    button1.setBackgroundColor(getResources().getColor(R.color.purple));
+                    button2.setBackgroundColor(getResources().getColor(R.color.purple));
+                }
+                else if (rand_1 >= 0.2 && rand_1 < 0.4){
+                    button1.setBackgroundColor(getResources().getColor(R.color.bright_green));
+                    button2.setBackgroundColor(getResources().getColor(R.color.bright_green));
+                }
+                else if (rand_1 >= 0.4  && rand_1 <0.6){
+                    button1.setBackgroundColor(getResources().getColor(R.color.blue));
+                    button2.setBackgroundColor(getResources().getColor(R.color.blue));
+                }
+                else if (rand_1 >= 0.6  && rand_1 <0.8){
+                    button1.setBackgroundColor(getResources().getColor(R.color.salmon));
+                    button2.setBackgroundColor(getResources().getColor(R.color.salmon));
+                }
+                else{
+                    button1.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                    button2.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
+            }
+        });
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2 = findViewById(R.id.button2);
+                num1 = findViewById(R.id.num1);
+                num2 = findViewById(R.id.num2);
+                points_txt = findViewById(R.id.points_txt);
+                int val1 = Integer.parseInt((num1.getText()).toString());
+                int val2 = Integer.parseInt((num2.getText()).toString());
+                if (val1 >= val2){
+                    points++;
+                    points_txt.setText("Points: " + points);
+                    Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    points--;
+                    points_txt.setText("Points: " + points);
+                    Toast.makeText(getApplicationContext(), "Incorrect!", Toast.LENGTH_SHORT).show();
+                }
+                new_rand_numbers();
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1 = findViewById(R.id.button1);
+                num1 = findViewById(R.id.num1);
+                num2 = findViewById(R.id.num2);
+                points_txt = findViewById(R.id.points_txt);
+                int val1 = Integer.parseInt((num1.getText()).toString());
+                int val2 = Integer.parseInt((num2.getText()).toString());
+                if (val1 <= val2){
+                    points++;
+                    points_txt.setText("Points: " + points);
+                    Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    points--;
+                    points_txt.setText("Points: " + points);
+                    Toast.makeText(getApplicationContext(), "Incorrect!", Toast.LENGTH_SHORT).show();
+                }
+                new_rand_numbers();
+            }
+        });
     }
     public void new_rand_numbers(){
         int rand_num_1 = (int) (Math.random()*(10 - 1)) + 1;
